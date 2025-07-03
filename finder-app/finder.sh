1e3b222860e08a585a080d84bf87945988f80569
kangminhee <kangminhee489@gmail.com>
2025-07-03 12:31:58 +0900

    docker install

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -18,4 +18,3 @@ file_count=$(find "$filesdir" -type f | wc -l)
 match_count=$(grep -r -R -I -F -o -- "$searchstr" "$filesdir" 2>/dev/null | wc -l)
 
 echo "The number of files are $file_count and the number of matching lines are $match_count"
-
